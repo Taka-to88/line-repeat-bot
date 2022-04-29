cbf13a619a9c11cddf78c037b285b3414d92dad8
Taka-to88 <takato-kondo@limedesignlab.com>
2022-04-29 21:44:16 +0900

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,51 @@
+import os
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
+from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+# 各クライアントライブラリのインスタンス作成
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+
+def lambda_handler(event, context):
+    if "x-line-signature" in event["headers"]:
+        signature = event["headers"]["x-line-signature"]
+    elif "X-Line-Signature" in event["headers"]:
+        signature = event["headers"]["x-line-signature"]
+    # リクエストボディを取得
+    body = event["body"]
+    ok_json = {"isBase64Encoded": False,
+               "statusCode": 200,
+               "headers": {},
+               "body": ""}
+    error_json = {"isBase64Encoded": False,
+                  "statusCode": 500,
+                  "headers": {},
+                  "body": "Error"}
+    # メッセージイベント
+
+    @handler.add(MessageEvent, message=TextMessage)
+    def message(line_event):
+        text = line_event.message.text
+        line_bot_api.reply_message(
+            line_event.reply_token, TextSendMessage(text=text))
+
+    try:
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        logger.error("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            logger.error("  %s: %s" % (m.property, m.message))
+        return error_json
+    except InvalidSignatureError:
+        return error_json
+
+    return ok_json
